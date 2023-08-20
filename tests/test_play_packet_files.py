04854dcec927ac151ca7a9fdab636ca36b6dba91
mefistotelis <mefistotelis@gmail.com>
2023-08-20 23:00:08 +0200

    packets: Some empty packet files to test missions

---
+++ tests/test_play_packet_files.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+""" Test for Syndicate Wars Port.
+
+    This test runs packet files on missions, starting the game for each packet
+    file.
+    Run it using `pytest` in project root folder.
+"""
+
+# Copyright (C) 2023 Mefistotelis <mefistotelis@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import glob
+import itertools
+import logging
+import os
+import re
+import sys
+import pathlib
+import pytest
+import subprocess
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+@pytest.mark.parametrize("pckt_inp_fn", [fn for fn in itertools.chain.from_iterable([ glob.glob(e, recursive=True) for e in (
+    './qdata/savegame/rc*m*.s01',
+  ) ]) if os.path.isfile(fn)] )
+def test_play_empty_packet_files(pckt_inp_fn):
+    """ Test of missions startup. Loads short packet file for each mission, the file soon executes quit packet.
+    """
+    LOGGER.info("Testcase file: {:s}".format(pckt_inp_fn))
+
+    pckt_path, pckt_filename = os.path.split(pckt_inp_fn)
+    pckt_path = pathlib.Path(pckt_path)
+    pckt_basename, pckt_fileext = os.path.splitext(pckt_filename)
+    log_fn = "{:s}.log".format(pckt_basename)
+    logs_out_path = "test_out"
+    match = re.search('^rc[0]*([0-9]+)m[0]*([0-9]+).s[0]*([0-9]+)$', pckt_filename)
+    assert match, "Test bug: Cannot parse packet file name \"{}\"".format(pckt_filename)
+    campgn = match.group(1)
+    missi = match.group(2)
+    pcktno = match.group(3)
+    logs_out_fn = os.sep.join([logs_out_path, log_fn])
+    if not os.path.exists(logs_out_path):
+        os.makedirs(logs_out_path)
+    # Run the game
+    command = [os.path.join(".", "swars"), "-W", "-q", "-m", "{},{}".format(campgn,missi), "-p", "{}".format(pcktno)]
+    LOGGER.info(' '.join(command))
+    subprocess.run(command)
+    os.replace("error.log", logs_out_fn)
+    log = open(logs_out_fn, "r").read()
+    assert "LbDataFree" in log, "Log file does not contain cleanup, execution did not ended normally"
